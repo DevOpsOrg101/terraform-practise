a3bb58eb54ba8218b1105dd68df102264a19d165
manosij majumder <ankur.majumder96@gmail.com>
2025-07-14 07:57:57 +0530

    added code

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.25.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "4556d065-e801-43d2-bae9-68a06a4a8886"
+
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-b17g1"
+  location = "West Europe"
+}
+
+
